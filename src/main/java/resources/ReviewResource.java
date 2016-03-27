4b1ab9d6b30b7d01ed8b4d0bcbf3d983f3950071
Sokkalingam SUBRAMANIAN <sokkalingam.subramanian@amadeus.com>
2016-03-27 11:02:33 -0400

    Image and Review Services

--- src/main/java/resources/ReviewResource.java
+++ src/main/java/resources/ReviewResource.java
@@ -2,7 +2,11 @@ package resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -11,6 +15,7 @@ import javax.ws.rs.core.MediaType;
 import models.Review;
 import services.ReviewService;
 
+@Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class ReviewResource {
 	
@@ -25,6 +30,23 @@ public class ReviewResource {
 	@Path("/{reviewId}")
 	public Review getReview(@PathParam("gemId") Integer gemId, @PathParam("reviewId") Integer reviewId) {
 		return reviewService.getReview(gemId, reviewId);
+	}
+	
+	@POST
+	public Review addReview(@PathParam("gemId") Integer gemId, Review review){
+		return reviewService.addReview(gemId, review);
+	}
+	
+	@PUT
+	@Path("/{reviewId}")
+	public Review updateReview(@PathParam("gemId") Integer gemId, @PathParam("reviewId") Integer reviewId, Review review) {
+		return reviewService.updateReview(gemId, reviewId, review);
+	}
+	
+	@DELETE
+	@Path("/{reviewId}")
+	public Review deleteReview(@PathParam("gemId") Integer gemId, @PathParam("reviewId") Integer reviewId) {
+		return reviewService.deleteReview(gemId, reviewId);
 	}
 	
 
--- src/main/java/services/GemService.java
+++ src/main/java/services/GemService.java
@@ -1,26 +1,48 @@
 package services;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 import databases.Database;
 import models.Gem;
-import models.Image;
-import models.Review;
 
 public class GemService {
 	
 	private Map<Integer, Gem> gems = Database.getGems();
 	
+	/*
+	 {
+		  "name": "Ruby",
+		  "specifications": "24 Carat pure diamond",
+		  "description": "Shines bright like a diamond",
+		  "quantity": 10,
+		  "canPurchase": true,
+		  "soldOut": false,
+		  "id": 1
+	  }
+	 */
+	
 	public GemService() {
-		Gem gem = new Gem();
-		gem.setId(1);
-		gem.setName("Ruby");
-		gems.put(gem.getId(), gem);
-		gem.setReviews(Arrays.asList(new Review(5, "This is the review body", "Sokka")));
-		gem.setImages(Arrays.asList(new Image("thumb", "img")));
+//		Gem gem = new Gem();
+//		gem.setId(1);
+//		gem.setName("Ruby");
+//		gem.setSpecifications("24 Carat pure diamond");
+//		gem.setDescription("Shines bright like a diamond");
+//		gem.setQuantity(10);
+//		gem.setCanPurchase(true);
+//		gem.setSoldOut(false);
+//		Map<Integer, Image> images = new HashMap<Integer, Image>();
+//		Image image = new Image("thumb.jpg", "image.jpg");
+//		image.setId(1);
+//		images.put(image.getId(), image);
+//		Map<Integer, Review> reviews = new HashMap<Integer, Review>();
+//		Review review = new Review(5, "Body of the review", "Author");
+//		review.setId(1);
+//		reviews.put(review.getId(), review);
+//		gem.setReviews(reviews);
+//		gem.setImages(images);
+//		gems.put(gem.getId(), gem);
 	}
 	
 	public List<Gem> getAllGems() {
