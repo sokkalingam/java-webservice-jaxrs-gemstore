d8f1dc5f0e84794a7525921e96efaec1be1aa965
Sokkalingam SUBRAMANIAN <sokkalingam.subramanian@amadeus.com>
2016-04-08 17:50:42 -0400

    Gem test data - images - names

--- src/main/java/services/GemService.java
+++ src/main/java/services/GemService.java
@@ -17,38 +17,8 @@ public class GemService {
 	
 	private Map<Integer, Gem> gems = Database.getGems();
 	
-	/*
-	 {
-		  "name": "Ruby",
-		  "specifications": "24 Carat pure diamond",
-		  "description": "Shines bright like a diamond",
-		  "quantity": 10,
-		  "canPurchase": true,
-		  "soldOut": false,
-		  "id": 1
-	  }
-	 */
-	
 	public GemService() {
-//		Gem gem = new Gem();
-//		gem.setId(1);
-//		gem.setName("Ruby");
-//		gem.setSpecifications("24 Carat pure diamond");
-//		gem.setDescription("Shines bright like a diamond");
-//		gem.setQuantity(10);
-//		gem.setCanPurchase(true);
-//		gem.setSoldOut(false);
-//		Map<Integer, Image> images = new HashMap<Integer, Image>();
-//		Image image = new Image("thumb.jpg", "image.jpg");
-//		image.setId(1);
-//		images.put(image.getId(), image);
-//		Map<Integer, Review> reviews = new HashMap<Integer, Review>();
-//		Review review = new Review(5, "Body of the review", "Author");
-//		review.setId(1);
-//		reviews.put(review.getId(), review);
-//		gem.setReviews(reviews);
-//		gem.setImages(images);
-//		gems.put(gem.getId(), gem);
+		
 	}
 	
 	public List<Gem> getAllGems() {
